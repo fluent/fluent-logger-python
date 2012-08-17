44254e39436dd38a2ef502c9bce370cb89c87d83
Kazuki Ohta <kazuki.ohta@gmail.com>
2012-08-17 23:00:14 +0900

    v0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = 'A Python logging handler for Fluentd event collector'
 
 setup(
   name='fluent-logger',
-  version='0.3.1',
+  version='0.3.2',
   description=desc,
   long_description=open(README).read(),
   package_dir={'fluent': 'fluent'},
