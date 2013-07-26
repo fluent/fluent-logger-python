954f823bdc7cecc79e42524ec15b20c96cbf53cd
Gilles Dartiguelongue <gilles.dartiguelongue@esiee.org>
2013-07-26 11:04:34 +0200

    Use dict.get instead of ternary statement

--- fluent/event.py
+++ fluent/event.py
@@ -5,6 +5,6 @@ class Event(object):
     def __init__(self, label, data, **kwargs):
         if not isinstance(data, dict) :
             raise Exception("data must be dict")
-        s = kwargs['sender'] if ('sender' in kwargs) else sender.get_global_sender()
-        timestamp = kwargs['time'] if ('time' in kwargs) else int(time.time())
+        s = kwargs.get('sender', sender.get_global_sender())
+        timestamp = kwargs.get('time', int(time.time())
         s.emit_with_time(label, timestamp, data)
