1dc7d8ffbf09172e3db8f659e8fb87ef32881bc3
Mac Ryan <quasipedia@gmail.com>
2014-09-12 14:56:14 +0200

    Patch import chain to support "basestring" in python3

--- fluent/handler.py
+++ fluent/handler.py
@@ -3,6 +3,23 @@
 import logging
 import socket
 
+# This is a patch to make the code cross-compatible between python 2 and 3
+# Source: http://www.rfk.id.au/blog/entry/preparing-pyenchant-for-python-3/
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str, bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 try:
     import simplejson as json
 except ImportError:
