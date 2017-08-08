a9d9cf327439405b283a8cbd8a2daff3706d264f
Masahiro Nakagawa <repeatedly@gmail.com>
2017-08-09 06:07:55 +0900

    v0.5.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ desc = 'A Python logging handler for Fluentd event collector'
 
 setup(
   name='fluent-logger',
-  version='0.5.0',
+  version='0.5.1',
   description=desc,
   long_description=open(README).read(),
   package_dir={'fluent': 'fluent'},
